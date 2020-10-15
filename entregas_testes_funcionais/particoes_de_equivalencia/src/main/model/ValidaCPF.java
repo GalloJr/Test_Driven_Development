5166986b0b1cf2ba567232966a76d8610de96823
Renato Gallo <renato.gallo@fatec.sp.gov.br>
2020-10-15 20:36:48 -0300

    Estado Verde

--- entregas_testes_funcionais/particoes_de_equivalencia/src/main/model/ValidaCPF.java
+++ entregas_testes_funcionais/particoes_de_equivalencia/src/main/model/ValidaCPF.java
@@ -5,12 +5,88 @@ import java.util.InputMismatchException;
 public class ValidaCPF {
 
     public static boolean isCPF(String CPF) {
-       // sequencia de numeros iguais = CPF Invalido
-        if ((CPF.length() != 11) ||
-        CPF.equals("11111111111")){
-            return (true);
+        if (CPF.equals("00000000000")) {
+            return(false);
         }
-        return (false);
+        if (CPF.equals("11111111111")) {
+            return(false);
+        }
+        if (CPF.equals("22222222222")) {
+            return(false);
+        }
+        if (CPF.equals("33333333333")) {
+            return(false);
+        }
+        if (CPF.equals("44444444444")) {
+            return(false);
+        }
+        if (CPF.equals("55555555555")) {
+            return(false);
+        }
+        if (CPF.equals("66666666666")) {
+            return(false);
+        }
+        if (CPF.equals("77777777777")) {
+            return(false);
+        }
+        if (CPF.equals("88888888888")) {
+            return(false);
+        }
+        if (CPF.equals("99999999999")) {
+            return(false);
+        }
+        if (CPF.length() != 11) {
+            return(false);
+        }
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+        // Calculo do 1º. Digito Verificador
+        sm = 0;
+        peso = 10;
+        for (i=0; i<9; i++) {
+        // converte o caractere do CPF em um numero:
+        // por exemplo, transforma o caractere '0' no inteiro 0
+        // (48 eh a posicao de '0' na tabela ASCII)
+        num = (int)(CPF.charAt(i) - 48);
+        sm = sm + (num * peso);
+        peso = peso - 1;
+        }
+
+        r = 11 - (sm % 11);
+        if ((r == 10) || (r == 11))
+            dig10 = '0';
+        else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+
+        // Calculo do 2o. Digito Verificador
+        sm = 0;
+        peso = 11;
+        for(i=0; i<10; i++) {
+        num = (int)(CPF.charAt(i) - 48);
+        sm = sm + (num * peso);
+        peso = peso - 1;
+        }
+
+        r = 11 - (sm % 11);
+        if ((r == 10) || (r == 11))
+            dig11 = '0';
+        else dig11 = (char)(r + 48);
+
+        // Verifica se os digitos calculados conferem com os digitos informados.
+        if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+            return(true);
+        else return(false);
+            } catch (InputMismatchException erro) {
+            return(false);
+        }
+        }
+
+        public static String imprimeCPF(String CPF) {
+        return(CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "." +
+        CPF.substring(6, 9) + "-" + CPF.substring(9, 11));
         
     }
 }
